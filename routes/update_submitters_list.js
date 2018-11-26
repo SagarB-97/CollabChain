026fa6354e543b909eb4eb32d3a57dade257d990
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2018-11-26 09:35:26 +0000

    Displays number of bathces completed by each executor. Made changes to accomodate characters in functions.

--- routes/update_submitters_list.js
+++ routes/update_submitters_list.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 
 const peerListFilePath = './peerserver/peerList.json';
 
+function decodeEntities(encodedString) {
+  var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+  var translate = {
+      "nbsp":" ",
+      "amp" : "&",
+      "quot": "\"",
+      "lt"  : "<",
+      "gt"  : ">"
+  };
+  return encodedString.replace(translate_re, function(match, entity) {
+      return translate[entity];
+  }).replace(/&#(\d+);/gi, function(match, numStr) {
+      var num = parseInt(numStr, 10);
+      return String.fromCharCode(num);
+  });
+}
+
 function createNewFileIfAbsent() {
   let obj = {};
   try {
@@ -17,7 +34,7 @@ router.post('/', function(req, res, next) {
   createNewFileIfAbsent();
   let obj = JSON.parse(fs.readFileSync(peerListFilePath));
   let id = req.body.id;
-  obj[id] = {id: req.body.id, task_title: req.body.task_title, js_function: req.body.js_function};
+  obj[id] = {id: req.body.id, task_title: req.body.task_title, js_function: decodeEntities(req.body.js_function)};
   fs.writeFileSync(peerListFilePath, JSON.stringify(obj));
   res.send('List Updated');
 });
