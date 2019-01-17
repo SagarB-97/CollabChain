62834aca078c39d76c244bcfd6a5cbc5e3ee5a61
Samvid Dharanikota <samvid25@yahoo.com>
2019-01-18 02:42:50 +0530

    Fix web3 errors

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "10.52.144.61",
       port: 7546,
       network_id: "*", // Match any network id
       gas: 4600000
