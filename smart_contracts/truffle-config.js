a0c886afd3f08ac3b161fc7f9baa770ec128ad6f
Samvid Dharanikota <samvid25@yahoo.com>
2019-01-18 00:28:20 +0530

    Add interaction with smart contract

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 7546,
       network_id: "*", // Match any network id
       gas: 4600000
     }
