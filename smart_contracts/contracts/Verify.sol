75ec412e9dffe446a840fd9e4fa3afc11366d4c2
Samvid Dharanikota <samvid25@yahoo.com>
2019-01-30 17:53:46 +0530

    Fix smart contract verification logic and payment

--- smart_contracts/contracts/Verify.sol
+++ smart_contracts/contracts/Verify.sol
@@ -12,6 +12,8 @@ contract Verify {
 
 	mapping(uint => process) processes;
 
+	event verified(address _from, bool success);
+
 	function add_process(uint _p_id, uint _n, uint[] memory _indices, 
                         uint[] memory _noutputs, uint _price) public payable returns (bool success) {
 		process memory p = process(msg.sender, _n, _indices, _noutputs, _price);
@@ -19,28 +21,41 @@ contract Verify {
 		return true;
 	}
 
-	function verify_output(uint _p_id, uint _m, uint[] memory _indices, uint[] memory _outputs) public returns (bool success) {
+	function verify_output(uint _p_id, uint _m, uint[] memory _indices, uint[] memory _outputs) public returns (string memory success) {
 		process memory p = processes[_p_id];
-		for(uint i = 0; i < p.n; i++)
+		uint i = 0;
+		for(i = 0; i < p.n; i++)
 		{
-			if(p.indices[i] > _indices[0])
+			if(p.indices[i] >= _indices[0])
+				break;
+		}
+		for(uint j = 0; j < _m; j++)
+		{
+			if( (p.indices[i] == _indices[j]) && (_outputs[j] != p.n_outputs[i]) )
 			{
-				for (uint j = 0; j < _m; j++)
-				{
-					if(p.indices[i] == _indices[j])
-					{
-						if(_outputs[j] == p.n_outputs[i])
-						{
-							msg.sender.transfer(p.price/p.n);
-							delete processes[_p_id];
-							return true;
-						}
-						else return false;
-					}
-				}
+				emit verified(msg.sender, false);
+				return "false";
+			}
+			else if( (p.indices[i] == _indices[j]) && (_outputs[j] == p.n_outputs[i]) )
+			{
+				i++;
+				if (i == p.n)
+					break;
 			}
 		}
-		return false;
+		msg.sender.transfer((p.price / p.n) * 1 ether);
+		emit verified(msg.sender, true);
+		return "true";
+	}
+
+	function show(uint d) public view returns (address a, uint[] memory i, uint[] memory o, uint p)
+	{
+		return (processes[d].submitter, processes[d].indices, processes[d].n_outputs, processes[d].price);
+	}
+
+	function getbalance() public view returns (uint a)
+	{
+		return (address(this).balance);
 	}
 
 }
--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "10.52.144.61",
+      host: "127.0.0.1",
       port: 7546,
       network_id: "*", // Match any network id
       gas: 4600000
