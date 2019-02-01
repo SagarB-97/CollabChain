651b6eebf01077898b9c781880c60c9f46e166da
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2019-02-01 10:33:50 +0000

    Added time calculation

--- Examples/factorisation/function.js
+++ Examples/factorisation/function.js
@@ -1,11 +1,8 @@
-obj = input;
 factor = [];
-outArr = [];
-for(var k = obj.start; k <= obj.end && k * k <= obj.num; k++) {
-	if(obj.num % k == 0) {
+for(var k = input.start; k <= input.end && k * k <= input.num; k++) {
+	if(input.num % k == 0) {
 		factor.push(k);
-		factor.push(obj.num / k);
+		factor.push(input.num / k);
 	}
 }
-outArr.push({Factor: factor});
-return {output: outArr};
+return {Factor: factor};
