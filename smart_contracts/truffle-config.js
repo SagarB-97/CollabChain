c2d161f481e4fc4309e8cf859dc360124a57b048
Samvid Dharanikota <samvid25@yahoo.com>
2019-02-01 15:42:12 +0530

    Fix event handler bugs

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      // host: "10.52.128.243",
-      host: "127.0.0.1",
+      host: "10.52.128.243",
+      // host: "127.0.0.1",
       port: 7546,
       network_id: "*", // Match any network id
       gas: 4600000
